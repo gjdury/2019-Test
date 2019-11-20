be6c816e2391be0ebca7514006173fd7913747e1
gjdury <gjdury@indiana.edu>
2019-11-20 15:32:50 -0500

    New test project

---
+++ src/FirstScript.R
@@ -0,0 +1,10 @@
+x=1:10000
+
+#Scripts in src folder
+#Graphs in the graphs folder
+#Raw data in the data folder
+
+#Using the same file structure allows for the use of relative filepaths rather than complete filepaths
+list.files("data") #lists the files in the folder called data
+list.files("../") #lists 
+
